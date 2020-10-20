2b73a829502c328d674fbc93765ffcf09b2f86c8
wyq977 <wangyq977@gmail.com>
2020-10-20 23:27:36 +0200

    update travis config

--- install-boost.sh
+++ install-boost.sh
@@ -2,7 +2,8 @@
 set -ex
 
 cd /tmp
-wget -c http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.gz -O - | sudo tar -xz
+wget http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.gz
+tar -xzvf boost_1_57_0.tar.gz
 cd boost_1_57_0 && ./bootstrap.sh --prefix=/usr/local && ./b2 install
 
 # clean up
