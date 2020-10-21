3efe8cceb7d17e43dd8cec9d26fc4b4ab00d8774
wyq977 <wangyq977@gmail.com>
2020-10-21 16:40:14 +0200

    add boost install script

--- install-boost.sh
+++ install-boost.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -ex
 
+BOOST_VERSION=${1:-1.57.0}
+BOOST_LIB=${2:-/usr/local}
+BOOST_VERSION_UNDERSCORE=$( echo ${BOOST_VERSION} | tr '.' '_' )
+
 cd /tmp
-wget http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.gz
-tar -xzvf boost_1_57_0.tar.gz
-cd boost_1_57_0 && ./bootstrap.sh --prefix=/usr/local && ./b2 install
+wget http://downloads.sourceforge.net/project/boost/boost/${BOOST_VERSION}/boost_${BOOST_VERSION_UNDERSCORE}.tar.gz
+tar -xzvf boost_${BOOST_VERSION_UNDERSCORE}.tar.gz
+cd boost_${BOOST_VERSION_UNDERSCORE} && ./bootstrap.sh --prefix=${BOOST_LIB} && ./b2 install
 
 # clean up
-cd /tmp && rm boost_1_57_0
+rm /tmp/boost_${BOOST_VERSION_UNDERSCORE}
