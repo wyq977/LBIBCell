6fffedca2a3927b2fab529ba938e5a7cfb02a52d
wyq977 <wangyq977@gmail.com>
2020-10-24 18:57:05 +0200

    make change

--- Untitled-1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -ex
-
-VTK_VERSION=${1:-5.10.1}
-VTK_LIB=${2:-/usr/local/lib}
-
-echo ${VTK_LIB}
-echo ${VTK_VERSION}
--- install-vtk.sh
+++ install-vtk.sh
@@ -8,6 +8,10 @@ VTK_LIB=${2:-/usr/local}
 git clone --single-branch --branch v$VTK_VERSION https://gitlab.kitware.com/vtk/vtk.git /tmp/vtk
 
 # out-of-source build
+# https://stackoverflow.com/questions/28761702/getting-error-glintptr-has-not-been-declared-when-building-vtk-on-linux
+# Not sure if these two options are needed
+# -D VTK_USE_RENDERING:BOOL=OFF \
+# -D VTK_USE_DISPLAY:BOOL=OFF \
 mkdir /tmp/vtk-build && cd /tmp/vtk-build
 cmake -Wno-dev \
 -D CMAKE_BUILD_TYPE:STRING=Release \
