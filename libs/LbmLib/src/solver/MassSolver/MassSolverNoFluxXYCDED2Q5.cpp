1b014c312eecc245b69533c1f2cbe9ec5ae40f3e
wyq977 <wangyq977@gmail.com>
2020-11-27 11:13:28 +0100

    Test boundary adding postAdvect

--- libs/LbmLib/src/solver/MassSolver/MassSolverNoFluxXYCDED2Q5.cpp
+++ libs/LbmLib/src/solver/MassSolver/MassSolverNoFluxXYCDED2Q5.cpp
@@ -162,57 +162,102 @@ void MassSolverNoFluxXYCDED2Q5::calculateMass(
     // no flux at y=0:
 #pragma omp parallel for schedule(dynamic)
     for (size_t itx = 0; itx <fluidGrid[0].size(); itx++) {
+        // get fluid velocity
+        const double ux = fluidGrid[0][itx]->getFluidSolver().getVelocity().x;
+        const double uy = fluidGrid[0][itx]->getFluidSolver().getVelocity().y;
+
         // look at BoundarySolverNoFluxD2Q5        
         for (auto cdes = fluidGrid[0][itx]->getCDESolvers().begin();
              cdes != fluidGrid[0][itx]->getCDESolvers().end();
              cdes++) {
                 // neighbour: y=1
                 auto nbcdes = fluidGrid[1][itx]->getCDESolvers().begin();
+                double Cf = (*nbcdes)->getC() / 6.0;
+
+                // get the velocity on fluidGrid
+                double feq = 0.0;
+                double fneq = 0.0;
                 double newDistribution = 0.0;
 
                 // E
-                newDistribution = (*nbcdes)->accessDistribution(E);
-                (*cdes)->setDistribution(E, newDistribution);
+                feq = Cf * (1 + 3 * ux);
+                fneq =
+                    (*nbcdes)->accessDistribution(getInverseDirection(E)) -
+                    (*nbcdes)->calculateEquilibrium(E);
+                newDistribution = feq + fneq;
+                (*nbcdes)->setDistribution(E, newDistribution);
                 // N
-                newDistribution = (*nbcdes)->accessDistribution(E);
-                (*cdes)->setDistribution(N, newDistribution);
+                feq = Cf * (1 + 3 * uy);
+                fneq =
+                    (*nbcdes)->accessDistribution(getInverseDirection(N)) -
+                    (*nbcdes)->calculateEquilibrium(N);
+                newDistribution = feq + fneq;
+                (*nbcdes)->setDistribution(N, newDistribution);
                 // W
-                newDistribution = (*nbcdes)->accessDistribution(E);
-                (*cdes)->setDistribution(W, newDistribution);
+                feq = Cf * (1 - 3 * ux);
+                fneq =
+                    (*nbcdes)->accessDistribution(getInverseDirection(W)) -
+                    (*nbcdes)->calculateEquilibrium(W);
+                newDistribution = feq + fneq;
+                (*nbcdes)->setDistribution(W, newDistribution);
                 // S
-                newDistribution = (*nbcdes)->accessDistribution(E);
-                (*cdes)->setDistribution(S, newDistribution);
-                // T
-                newDistribution = (*nbcdes)->accessDistribution(E);
-                (*cdes)->setDistribution(S, newDistribution);
+                feq = Cf * (1 - 3 * uy);
+                fneq =
+                    (*nbcdes)->accessDistribution(getInverseDirection(S)) -
+                    (*nbcdes)->calculateEquilibrium(S);
+                newDistribution = feq + fneq;
+                (*nbcdes)->setDistribution(S, newDistribution);
             }
         }
 
     // no flux at y=LY:
 #pragma omp parallel for schedule(dynamic)
     for (size_t itx = 0; itx <fluidGrid[0].size(); itx++) {
+        // get fluid velocity
+        const double ux = fluidGrid[LY - 1][itx]->getFluidSolver().getVelocity().x;
+        const double uy = fluidGrid[LY - 1][itx]->getFluidSolver().getVelocity().y;
+
         // look at BoundarySolverNoFluxD2Q5        
         for (auto cdes = fluidGrid[LY - 1][itx]->getCDESolvers().begin();
              cdes != fluidGrid[LY - 1][itx]->getCDESolvers().end();
              cdes++) {
                 // neighbour: y=L-1
                 auto nbcdes = fluidGrid[LY - 2][itx]->getCDESolvers().begin();
+                double Cf = (*nbcdes)->getC() / 6.0;
+
+                // get the velocity on fluidGrid
+                double feq = 0.0;
+                double fneq = 0.0;
                 double newDistribution = 0.0;
+
                 // E
-                newDistribution = (*nbcdes)->accessDistribution(E);
-                (*cdes)->setDistribution(E, newDistribution);
+                feq = Cf * (1 + 3 * ux);
+                fneq =
+                    (*nbcdes)->accessDistribution(getInverseDirection(E)) -
+                    (*nbcdes)->calculateEquilibrium(E);
+                newDistribution = feq + fneq;
+                (*nbcdes)->setDistribution(E, newDistribution);
                 // N
-                newDistribution = (*nbcdes)->accessDistribution(E);
-                (*cdes)->setDistribution(N, newDistribution);
+                feq = Cf * (1 + 3 * uy);
+                fneq =
+                    (*nbcdes)->accessDistribution(getInverseDirection(N)) -
+                    (*nbcdes)->calculateEquilibrium(N);
+                newDistribution = feq + fneq;
+                (*nbcdes)->setDistribution(N, newDistribution);
                 // W
-                newDistribution = (*nbcdes)->accessDistribution(E);
-                (*cdes)->setDistribution(W, newDistribution);
+                feq = Cf * (1 - 3 * ux);
+                fneq =
+                    (*nbcdes)->accessDistribution(getInverseDirection(W)) -
+                    (*nbcdes)->calculateEquilibrium(W);
+                newDistribution = feq + fneq;
+                (*nbcdes)->setDistribution(W, newDistribution);
                 // S
-                newDistribution = (*nbcdes)->accessDistribution(E);
-                (*cdes)->setDistribution(S, newDistribution);
-                // T
-                newDistribution = (*nbcdes)->accessDistribution(E);
-                (*cdes)->setDistribution(S, newDistribution);
+                feq = Cf * (1 - 3 * uy);
+                fneq =
+                    (*nbcdes)->accessDistribution(getInverseDirection(S)) -
+                    (*nbcdes)->calculateEquilibrium(S);
+                newDistribution = feq + fneq;
+                (*nbcdes)->setDistribution(S, newDistribution);
             }
         }
 
